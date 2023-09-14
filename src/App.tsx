66f9207bac04ee328e6f5f8ddc1b2f5d0785519e
Seung Lee <leesh2985@naver.com>
2023-09-14 21:41:52 +0900

    날라간거 다시 올림

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-import Mypage from './page/MyPage/MyInfo';
 import Main from './page/Main/Main';
-import MainLayout from './MainLayout';  
-
+import MainLayout from './MainLayout';
+import Login from './page/LoginPage/Login';
+import EditMyInfo from './page/mypage/EditMyInfo';
+import MyInfo from './page/mypage/MyInfo';
 
 function App() {
   return (
     <BrowserRouter>
-        <Login />
+      <Login />
       <Routes>
         {/* Header,Footer 보여주고 싶은 컴포넌트 */}
         <Route element={<MainLayout />}>
           <Route path="/main" element={<Main />} />
-          <Route path="/myinfo" element={<Mypage />} />
+          <Route path="/myinfo" element={<MyInfo />} />
         </Route>
         {/* Header,Footer을 안 보여주고 싶은 컴포넌트 */}
         <Route path="/editmyinfo" element={<EditMyInfo />} />
