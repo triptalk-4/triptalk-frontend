eec98be5f147cd4bfc632e729ef2330104e6a800
DESKTOP-JVEBI2D\JIN <parkjin0115@naver.com>
2023-09-14 16:07:44 +0900

    #Feat - LoginPage 메인화면 import

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Mypage from './page/MyPage/MyPage';
-import Main from './page/Main/Main';
+import Main from './page/MainPage/Main';
+import Login from './page/LoginPage/Login';
 
 function App() {
   return (
     <BrowserRouter>
+        <Login />
       <Routes>
         <Route path="/main" element={<Main />} />
         <Route path="/mypage" element={<Mypage />} />
