b8242a6636f80b1b1d8b620a815dffb33a96d5b9
Seung Lee <leesh2985@naver.com>
2023-09-14 20:05:17 +0900

    주석에 적힌대로 헤더랑 푸터 보여줄 컴포넌트 안보여줄 컴포넌트 분리 시켜놨어요

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Mypage from './page/MyPage/MyPage';
+import Mypage from './page/MyPage/MyInfo';
 import Main from './page/Main/Main';
+import MainLayout from './MainLayout';
+import EditMyInfo from './page/MyPage/EditMyInfo';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/main" element={<Main />} />
-        <Route path="/mypage" element={<Mypage />} />
+        {/* Header,Footer 보여주고 싶은 컴포넌트 */}
+        <Route element={<MainLayout />}>
+          <Route path="/main" element={<Main />} />
+          <Route path="/myinfo" element={<Mypage />} />
+        </Route>
+        {/* Header,Footer을 안 보여주고 싶은 컴포넌트 */}
+        <Route path="/editmyinfo" element={<EditMyInfo />} />
       </Routes>
     </BrowserRouter>
   );
