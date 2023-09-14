20d57355663c763a927a1dc88ed764e1eae6dc5b
Jeong-DongYoung <school2013_@naver.com>
2023-09-14 12:40:39 +0900

    메인

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Mypage from './page/mypage/MyPage';
+import Main from './page/Main/Main';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/main" element={<Main />} />
         <Route path="/mypage" element={<Mypage />} />
       </Routes>
     </BrowserRouter>
--- src/page/mypage/MyPage.tsx
+++ src/page/mypage/MyPage.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function Mypage() {
   return <div>내정보</div>;
 }
