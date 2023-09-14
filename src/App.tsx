63d62ed4bc0b6622cc0d54c02eab766b8e8d5ba7
Jeong-DongYoung <school2013_@naver.com>
2023-09-14 12:24:50 +0900

    수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import MyPage from "./page/mypage/MyPage";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Mypage from './page/mypage/MyPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/mypage" element={<Mypage />} />
       </Routes>
     </BrowserRouter>
   );
--- src/page/mypage/MyPage.tsx
+++ src/page/mypage/MyPage.tsx
@@ -1,3 +1,3 @@
-export default function mypage() {
+export default function Mypage() {
   return <div>내정보</div>;
 }
