e76367deaa633164c147555a1a9b6b6d3ecb566c
DESKTOP-JVEBI2D\JIN <parkjin0115@naver.com>
2023-09-15 12:07:34 +0900

    충돌

--- src/page/mypage/seung.tsx
+++ src/page/mypage/seung.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
 export default function seung() {
-  return <div>seung</div>;
+  return (
+    <>
+      <h1>충돌</h1>
+      <h1>충돌</h1>
+      <h1>충돌</h1>
+      <h1>충돌</h1>
+      <h1>충돌</h1>
+      <h1>충돌</h1>
+      <h1>충돌</h1>
+    </>
+  )
 }
