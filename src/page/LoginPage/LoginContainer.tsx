84b53a4c6305b4926aff5df654e3a71ba6dc7b7f
DESKTOP-JVEBI2D\JIN <parkjin0115@naver.com>
2023-09-18 21:38:55 +0900

    #Design: LoginPage ui작업

---
+++ src/page/LoginPage/LoginContainer.tsx
@@ -0,0 +1,67 @@
+import styled from "styled-components"
+import { MAIN_COLOR } from "../../color/color"
+import LoginForm from "./LoginForm"
+
+function Login () {
+  return(
+    <CenteredContainer>
+        <Container>
+          <TitleWrap>
+            <Title>
+              Trip Talk  
+            </Title>
+            <SubTitle>국내의 여행자들을 위한 <br />여행리뷰 커뮤니티</SubTitle>
+          </TitleWrap>
+          <LoginWrap>
+            <LoginForm />
+          </LoginWrap>
+        </Container>
+    </CenteredContainer>
+  )
+}
+
+export default Login
+
+const CenteredContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Container = styled.div`
+  width: 100%;
+  height: 559px;
+  display: flex;
+`
+
+const TitleWrap = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
+const Title = styled.div`
+  color: ${MAIN_COLOR};
+  font-size: 10rem;
+  font-weight: bold;
+  word-spacing: -50px;
+  margin-top: 100px;
+  @media (width: 1113px) {
+    font-size: 8.125rem;
+  }
+`
+const SubTitle = styled.p `
+  font-size: 1.2rem;
+  margin-bottom: 120px;
+`
+
+const LoginWrap = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
