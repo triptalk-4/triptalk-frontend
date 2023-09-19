64335727cde38011716fbb7276fb3060697866e9
Jeong-DongYoung <school2013_@naver.com>
2023-09-19 17:41:24 +0900

    #Docs : Footer 추가

---
+++ src/component/Footer.tsx
@@ -0,0 +1,98 @@
+import styled from 'styled-components';
+
+function Footer() {
+  return (
+    <footer>
+      <FooterContainer>
+        <LogoImg src="public/img/footer_logo.png" />
+        <DescriptionContainer>
+          <CompanyTitle>
+            COMPANY
+            <CompanyDescription>
+              <DescriptionSpan>(주)사조사조</DescriptionSpan>
+              <DescriptionSpan>FE:이승현 박진 정동영</DescriptionSpan>
+              <DescriptionSpan> BF:김도욱 안재훈 이수정</DescriptionSpan>
+              <DescriptionSpan>사업자번호 : 202-30-90603</DescriptionSpan>
+            </CompanyDescription>
+          </CompanyTitle>
+          <EtcTitle>
+            ETC
+            <EtcDescription>
+              <DescriptionSpan>공지사항</DescriptionSpan>
+              <DescriptionSpan>이용약관</DescriptionSpan>
+              <DescriptionSpan>개인정보처리방침</DescriptionSpan>
+              <DescriptionSpan>FAQ</DescriptionSpan>
+              <DescriptionSpan>환불규정</DescriptionSpan>
+              <DescriptionSpan>채용</DescriptionSpan>
+            </EtcDescription>
+          </EtcTitle>
+          <ContactTitle>
+            CONTACT
+            <ContactDescription>
+              <DescriptionSpan>고객센터 : 02-023-0906</DescriptionSpan>
+              <DescriptionSpan>오전 11시~ 저녁 6시까지 운영</DescriptionSpan>
+              <DescriptionSpan>주말 및 공휴일, 점심시간 (12:00~13:00) 제외</DescriptionSpan>
+            </ContactDescription>
+          </ContactTitle>
+        </DescriptionContainer>
+      </FooterContainer>
+    </footer>
+  );
+}
+
+export default Footer;
+
+const FooterContainer = styled.div`
+  width: 100%;
+  height: 442px;
+  background-color: #2a2929;
+  color: white;
+  padding-left: 100px;
+`;
+
+const LogoImg = styled.img`
+  margin-top: 80px;
+  width: 300px;
+  height: 70px;
+`;
+
+const DescriptionContainer = styled.div`
+  width: 100%;
+  height: 400px;
+  display: flex;
+`;
+
+const CompanyTitle = styled.div`
+  font-size: 25px;
+  margin: 30px 120px 0 0;
+`;
+
+const CompanyDescription = styled.div`
+  font-size: 16px;
+  margin-top: 20px;
+`;
+
+const EtcTitle = styled.div`
+  font-size: 25px;
+  margin: 30px 120px 0 0;
+`;
+
+const EtcDescription = styled.div`
+  font-size: 16px;
+  margin-top: 20px;
+`;
+
+const DescriptionSpan = styled.span`
+  display: block;
+  width: 100%;
+`;
+
+const ContactTitle = styled.div`
+  font-size: 25px;
+  margin-top: 30px;
+`;
+
+const ContactDescription = styled.div`
+  font-size: 16px;
+  margin-top: 20px;
+`;
--- src/page/Main/Main.tsx
+++ src/page/Main/Main.tsx
@@ -1,3 +1,96 @@
-export default function Main() {
-  return <div>메인테스트중</div>;
-}
+import styled from 'styled-components';
+import { ImArrowRight } from 'react-icons/im';
+
+const Main = () => {
+  return (
+    <MainContainer>
+      <Title>
+        <TopPlan>
+          <TopPlanTitle>이달의 인기 PLAN</TopPlanTitle>
+          <TopPlanDescription>신선한 가을향이</TopPlanDescription>
+          <TopPlanDescription>느껴지는 9월에</TopPlanDescription>
+          <TopPlanDescription>
+            인기 포스트
+            <ArrowIconWrapper>
+              <ImArrowRight />
+            </ArrowIconWrapper>
+          </TopPlanDescription>
+        </TopPlan>
+        <HotPlace>
+          <HotPlaceTitle>
+            자주 방문한
+            <HotPlaceWithMargin>장소</HotPlaceWithMargin>
+          </HotPlaceTitle>
+          <HotPlaceDescription>끝나가는 여름</HotPlaceDescription>
+          <HotPlaceDescription>마지막 물놀이</HotPlaceDescription>
+          <HotPlaceDescriptionWithMargin> 어떠세요?</HotPlaceDescriptionWithMargin>
+        </HotPlace>
+      </Title>
+    </MainContainer>
+  );
+};
+
+const Title = styled.div``;
+const MainContainer = styled.div`
+  width: 100%;
+  height: 1000px;
+`;
+
+const TopPlan = styled.div`
+  width: 100%;
+  height: 400px;
+  margin: 100px 0;
+  padding-left: 100px;
+  font-weight: bold;
+  font-size: 24px;
+`;
+
+const HotPlace = styled.div`
+  width: 100%;
+  height: 400px;
+  padding-right: 100px;
+  font-weight: bold;
+  font-size: 24px;
+`;
+
+const TopPlanTitle = styled.div`
+  margin-bottom: 20px;
+`;
+
+const HotPlaceTitle = styled.div`
+  margin-bottom: 20px;
+`;
+
+const HotPlaceWithMargin = styled.span`
+  display: block;
+  margin-left: 66px;
+`;
+
+const TopPlanDescription = styled.span`
+  display: block;
+  font-weight: normal;
+  font-size: 16px;
+`;
+
+const HotPlaceDescription = styled.span`
+  display: block;
+  font-weight: normal;
+  font-size: 16px;
+  margin-left: 20px;
+`;
+
+const HotPlaceDescriptionWithMargin = styled.span`
+  display: block;
+  font-weight: normal;
+  font-size: 16px;
+  margin-left: 43px;
+`;
+
+const ArrowIconWrapper = styled.span`
+  vertical-align: middle;
+  display: inline-block;
+  margin-top: 2px;
+  margin-left: 5px;
+`;
+
+export default Main;
