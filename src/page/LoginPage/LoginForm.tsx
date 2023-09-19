9b224feb66bb808287f36defc120e335822d0ba8
DESKTOP-JVEBI2D\JIN <parkjin0115@naver.com>
2023-09-19 21:43:46 +0900

    #Feat : 로그인 기능 추가 + 로그인 시 메인화면으로 이동 기능 추가

--- src/page/LoginPage/LoginForm.tsx
+++ src/page/LoginPage/LoginForm.tsx
@@ -1,17 +1,62 @@
 import styled, { css } from 'styled-components';
 import { MAIN_COLOR } from "../../color/color"
+import { Link, useNavigate } from 'react-router-dom';
+import { SetStateAction, useState } from 'react';
+
+interface UserData {
+  id: number;
+  email: string;
+  password: string;
+}
+
+const userData: UserData[] = [
+  {id:1 , email: 'user1@naver.com', password: 'Password123'},
+  {id:2 , email: 'user2@naver.com', password: 'Password456'},
+  {id:3 , email: 'user3@naver.com', password: 'Password789'},
+]
 
 const LoginForm = () => {
+  
+  const naviget = useNavigate()
+  const [userEmail, setUserEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmail = (e: { target: { value: SetStateAction<string>; }; }) => {
+    setUserEmail(e.target.value)
+  }
+
+  const handlePw = (e: { target: { value: SetStateAction<string>; }; }) => {
+    setPassword(e.target.value)
+  }
+
+
+  const handleLogin = () => {
+    const user = userData.find((user) => user.email === userEmail && user.password === password)
+    if(user) {
+      alert('로그인 되었습니다.')
+      setUserEmail('')
+      setPassword('')
+      naviget('/main')
+    } else{
+      alert('유효하지 않은 사용자 정보입니다.')
+    }
+  }
+
   return (
     <Container>
       <Kakao>카카오톡 로그인</Kakao>
       <OrDivider>
         <span>또는</span>
       </OrDivider>
-      <InputEmail type="email" placeholder="이메일" />
+      <InputEmail type="email" placeholder="이메일" value={userEmail} onChange={handleEmail}/>
       <br />
-      <InputPw type="password" placeholder="비밀번호" />
-      <LoginButton>로그인</LoginButton>
+      <InputPw type="password" placeholder="비밀번호" value={password} onChange={handlePw}/>
+      <LoginButton onClick={handleLogin}>로그인</LoginButton>
+      <Div>
+        <span>아이디 찾기</span>
+        <span>비밀번호 찾기</span>
+        <Link to="/signup">회원가입</Link>
+      </Div>
     </Container>
   );
 };
@@ -34,6 +79,7 @@ const Kakao = styled.div`
   height: 56px;
   line-height: 56px;
   margin-bottom: 32px;
+  cursor: pointer;
 `;
 
 const OrDivider = styled.div`
@@ -64,6 +110,9 @@ const commonInputStyle = css`
   border-radius: 15px;
   padding-left: 21px;
   font-size: 1.2rem;
+  &:focus{
+    outline: none;
+  }
 `;
 
 const InputEmail = styled.input`
@@ -77,10 +126,22 @@ const InputPw = styled.input`
 const LoginButton = styled.button`
   width: 100%;
   height: 80px;
+  color: #fff;
   background-color: ${MAIN_COLOR};
   border: none;
+  cursor: pointer;
   border-radius: 15px;
   margin-top: 36px;
   font-size: 1.5rem;
-  color: #fff;
+`
+const Div = styled.div`
+  width: 100%;
+  margin-top: 8px;
+  font-size: 0.9rem;
+  span{
+    margin-right: 10px;
+  }
+  a:visited{
+  color: inherit
+  }
 `
