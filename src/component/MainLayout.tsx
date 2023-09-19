11847f8e254a453e274b1cd4613a9fd63d6037fe
Jeong-DongYoung <school2013_@naver.com>
2023-09-19 17:22:12 +0900

    # Docs : Footer 추가

--- src/component/MainLayout.tsx
+++ src/component/MainLayout.tsx
@@ -1,12 +1,14 @@
 import Header from './Header';
 
 import { Outlet } from 'react-router-dom';
+import Footer from './Footer';
 
 export default function MainLayout() {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 }
