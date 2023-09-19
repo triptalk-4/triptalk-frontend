bd01984a1c54eee114a5cbd4ed9c1d81f30e68b1
Seung Lee <leesh2985@naver.com>
2023-09-19 12:55:50 +0900

    # Design : 컬러 추가

--- src/color/color.ts
+++ src/color/color.ts
@@ -1,6 +1,7 @@
 export const MAIN_COLOR = '#F46222';
 export const YELLOW_COLOR = '#FFC121';
 export const LIGHT_ORANGE_COLOR = '#FB8D5D';
+export const SUPER_LIGHT_ORANGE_COLOR = '#ffcfbb';
 export const GRAY_COLOR = '#8D8D8D';
 export const LIGHT_GRAY_COLOR = '#D9D9D9';
 export const DEFAULT_FONT_COLOR = '#242424';
