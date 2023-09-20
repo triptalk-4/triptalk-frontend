eeba295c5854aa6888b601ec8f35d55702968c84
Jeong-DongYoung <school2013_@naver.com>
2023-09-20 18:03:22 +0900

    #Docs : 와이어 프레임 변경으로인한 수정 및 캐러셀 추가

--- src/page/Main/Main.tsx
+++ src/page/Main/Main.tsx
@@ -1,96 +1,67 @@
 import styled from 'styled-components';
-import { ImArrowRight } from 'react-icons/im';
+import MainCarousel from '../../component/Carousel/MainCarousel';
 
 const Main = () => {
   return (
     <MainContainer>
-      <Title>
-        <TopPlan>
-          <TopPlanTitle>이달의 인기 PLAN</TopPlanTitle>
-          <TopPlanDescription>신선한 가을향이</TopPlanDescription>
-          <TopPlanDescription>느껴지는 9월에</TopPlanDescription>
-          <TopPlanDescription>
-            인기 포스트
-            <ArrowIconWrapper>
-              <ImArrowRight />
-            </ArrowIconWrapper>
-          </TopPlanDescription>
-        </TopPlan>
-        <HotPlace>
-          <HotPlaceTitle>
-            자주 방문한
-            <HotPlaceWithMargin>장소</HotPlaceWithMargin>
-          </HotPlaceTitle>
-          <HotPlaceDescription>끝나가는 여름</HotPlaceDescription>
-          <HotPlaceDescription>마지막 물놀이</HotPlaceDescription>
-          <HotPlaceDescriptionWithMargin> 어떠세요?</HotPlaceDescriptionWithMargin>
-        </HotPlace>
-      </Title>
+      <TopPosts>
+        <TopTitle>좋아요 TOP6</TopTitle>
+        <MainCarousel />
+      </TopPosts>
+      <MainImgContainer>
+        <MainImageWrapper>
+          <MainImage src="public/img/Carousel.png" alt="" />
+          <MainImageCaption>여행지를 자랑해주세요</MainImageCaption>
+        </MainImageWrapper>
+      </MainImgContainer>
     </MainContainer>
   );
 };
 
-const Title = styled.div``;
 const MainContainer = styled.div`
   width: 100%;
-  height: 1000px;
+  height: 100%;
 `;
 
-const TopPlan = styled.div`
-  width: 100%;
+const TopPosts = styled.div`
+  width: 80%;
   height: 400px;
-  margin: 100px 0;
-  padding-left: 100px;
-  font-weight: bold;
-  font-size: 24px;
+  margin: 0 auto;
 `;
 
-const HotPlace = styled.div`
+const TopTitle = styled.div`
+  padding-left: 40%;
+  margin: 5% 0;
+  font-size: 30px;
+  font-weight: 700;
+`;
+
+const MainImgContainer = styled.div`
   width: 100%;
-  height: 400px;
-  padding-right: 100px;
-  font-weight: bold;
-  font-size: 24px;
+  height: 300px;
+  margin-top: 20%;
 `;
 
-const TopPlanTitle = styled.div`
-  margin-bottom: 20px;
+const MainImageWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 300px;
 `;
 
-const HotPlaceTitle = styled.div`
-  margin-bottom: 20px;
+const MainImage = styled.img`
+  width: 100%;
+  height: 100%;
 `;
 
-const HotPlaceWithMargin = styled.span`
-  display: block;
-  margin-left: 66px;
-`;
-
-const TopPlanDescription = styled.span`
-  display: block;
-  font-weight: normal;
+const MainImageCaption = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  padding: 5px;
   font-size: 16px;
-`;
-
-const HotPlaceDescription = styled.span`
-  display: block;
-  font-weight: normal;
-  font-size: 16px;
-  margin-left: 20px;
-`;
-
-const HotPlaceDescriptionWithMargin = styled.span`
-  display: block;
-  font-weight: normal;
-  font-size: 16px;
-  margin-left: 43px;
-`;
-
-const ArrowIconWrapper = styled.span`
-  vertical-align: middle;
-  display: inline-block;
-  margin-top: 2px;
-  margin-left: 5px;
+  transform: translate(-50%, -50%);
 `;
 
 export default Main;
