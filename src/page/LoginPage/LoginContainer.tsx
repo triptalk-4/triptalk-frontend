35c5a8dc266e7f24903d8c6820f8865ef07279b2
Jeong-DongYoung <school2013_@naver.com>
2023-09-20 18:02:24 +0900

    #Docs : 저장방식 차이로인하여 다시금 저장

--- src/page/LoginPage/LoginContainer.tsx
+++ src/page/LoginPage/LoginContainer.tsx
@@ -1,26 +1,27 @@
-import styled from "styled-components"
-import { MAIN_COLOR } from "../../color/color"
-import LoginForm from "./LoginForm"
+import styled from 'styled-components';
+import { MAIN_COLOR } from '../../color/color';
+import LoginForm from './LoginForm';
 
-function Login () {
-  return(
+function Login() {
+  return (
     <CenteredContainer>
-        <Container>
-          <TitleWrap>
-            <Title>
-              Trip Talk  
-            </Title>
-            <SubTitle>국내의 여행자들을 위한 <br />여행리뷰 커뮤니티</SubTitle>
-          </TitleWrap>
-          <LoginWrap>
-            <LoginForm />
-          </LoginWrap>
-        </Container>
+      <Container>
+        <TitleWrap>
+          <Title>Trip Talk</Title>
+          <SubTitle>
+            국내의 여행자들을 위한 <br />
+            여행리뷰 커뮤니티
+          </SubTitle>
+        </TitleWrap>
+        <LoginWrap>
+          <LoginForm />
+        </LoginWrap>
+      </Container>
     </CenteredContainer>
-  )
+  );
 }
 
-export default Login
+export default Login;
 
 const CenteredContainer = styled.div`
   width: 100%;
@@ -28,20 +29,20 @@ const CenteredContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const Container = styled.div`
   width: 100%;
   height: 559px;
   display: flex;
-`
+`;
 
 const TitleWrap = styled.div`
   width: 50%;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-`
+`;
 const Title = styled.div`
   color: ${MAIN_COLOR};
   font-size: 10rem;
@@ -51,11 +52,11 @@ const Title = styled.div`
   @media (width: 1113px) {
     font-size: 8.125rem;
   }
-`
-const SubTitle = styled.p `
+`;
+const SubTitle = styled.p`
   font-size: 1.2rem;
   margin-bottom: 120px;
-`
+`;
 
 const LoginWrap = styled.div`
   width: 50%;
@@ -63,5 +64,4 @@ const LoginWrap = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-`
-
+`;
