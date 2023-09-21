9ef876e10a223761e1a28b3fdd526cf714dcb872
Jeong-DongYoung <school2013_@naver.com>
2023-09-21 16:05:53 +0900

    # Docs : Main 페이지 작성

--- src/page/MainPage/Main.tsx
+++ src/page/MainPage/Main.tsx
@@ -1,3 +1,67 @@
-export default function Main() {
-  return <div>메인</div>;
-}
+import styled from 'styled-components';
+import MainCarousel from '../../component/Carousel/MainCarousel';
+
+const Main = () => {
+  return (
+    <MainContainer>
+      <TopPosts>
+        <TopTitle>좋아요 TOP6</TopTitle>
+        <MainCarousel />
+      </TopPosts>
+      <MainImgContainer>
+        <MainImageWrapper>
+          <MainImage src="public/img/Carousel.png" alt="" />
+          <MainImageCaption>여행지를 자랑해주세요</MainImageCaption>
+        </MainImageWrapper>
+      </MainImgContainer>
+    </MainContainer>
+  );
+};
+
+const MainContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const TopPosts = styled.div`
+  width: 80%;
+  height: 400px;
+  margin: 0 auto;
+`;
+
+const TopTitle = styled.div`
+  padding-left: 40%;
+  margin: 5% 0;
+  font-size: 30px;
+  font-weight: 700;
+`;
+
+const MainImgContainer = styled.div`
+  width: 100%;
+  height: 300px;
+  margin-top: 20%;
+`;
+
+const MainImageWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 300px;
+`;
+
+const MainImage = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+const MainImageCaption = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  padding: 5px;
+  font-size: 16px;
+  transform: translate(-50%, -50%);
+`;
+
+export default Main;
