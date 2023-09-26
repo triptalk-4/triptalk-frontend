65520025c856422f4d4a968fa64937155193d888
Seung Lee <leesh2985@naver.com>
2023-09-26 21:18:30 +0900

    # Design : 버튼 스타일 변경을 위해 생성

---
+++ src/page/TravelPage/CustomPrevArrow.tsx
@@ -0,0 +1,29 @@
+import { BsArrowLeftCircleFill } from 'react-icons/bs';
+import styled from 'styled-components';
+import { GRAY_COLOR, LIGHT_ORANGE_COLOR, MAIN_COLOR } from '../../color/color';
+
+interface CustomPrevArrowProps {
+  onClick?: () => void;
+  disabled?: boolean; // 버튼 비활성화 상태
+}
+
+export default function CustomPrevArrow({ onClick, disabled }: CustomPrevArrowProps) {
+  return (
+    <CustomArrowButton onClick={onClick} disabled={disabled}>
+      <BsArrowLeftCircleFill />
+    </CustomArrowButton>
+  );
+}
+
+const CustomArrowButton = styled.button`
+  color: ${props => (props.disabled ? GRAY_COLOR : LIGHT_ORANGE_COLOR)};
+  background-color: transparent;
+  border: none;
+  font-size: 25px;
+  cursor: ${props => (props.disabled ? 'default' : 'pointer')}; // 커서 포인터 설정
+
+  &:not(:disabled):hover {
+    // 버튼이 활성화 상태이고 호버하는 경우에만 스타일 적용
+    color: ${MAIN_COLOR};
+  }
+`;
