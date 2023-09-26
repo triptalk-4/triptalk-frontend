1f5ac5ddfbf27d7b37491448d42ed3317a4497ec
Seung Lee <leesh2985@naver.com>
2023-09-26 21:18:08 +0900

    # Design : 레이아웃 정리 및 객체 생성

--- src/page/TravelPage/TravelCarousel.tsx
+++ src/page/TravelPage/TravelCarousel.tsx
@@ -1,54 +1,105 @@
 import { Link } from 'react-router-dom';
 import Slider from 'react-slick';
 import styled, { css } from 'styled-components';
+import CustomNextArrow from './CustomNextArrow';
+import CustomPrevArrow from './CustomPrevArrow';
+import { useState } from 'react';
+
+interface PostItem {
+  id: number;
+  imgSrc: string;
+  title: string;
+  address: string;
+  date: string;
+  nickname: string;
+}
+
+const postdata: PostItem[] = [
+  {
+    id: 1,
+    imgSrc: 'img/Travelimg1.jpg',
+    title: '디저트 여행',
+    address: '대전광역시 중구 대종로 480번길 15',
+    date: '23.09.11',
+    nickname: '도토리',
+  },
+  {
+    id: 2,
+    imgSrc: 'img/Travelimg2.jpg',
+    title: '디저트 여행2',
+    address: '대전광역시 중구 대종로 480번길 15',
+    date: '23.09.12',
+    nickname: '도토리12',
+  },
+  {
+    id: 3,
+    imgSrc: 'img/Travelimg5.jpg',
+    title: '디저트 여행3',
+    address: '대전광역시 중구 대종로 480번길 15',
+    date: '23.09.13',
+    nickname: '도토리23',
+  },
+  {
+    id: 4,
+    imgSrc: 'img/Travelimg2.jpg',
+    title: '디저트 여행',
+    address: '대전광역시 중구 대종로 480번길 15',
+    date: '23.09.14',
+    nickname: '도토리34',
+  },
+  {
+    id: 5,
+    imgSrc: 'img/Travelimg5.jpg',
+    title: '디저트 여행2',
+    address: '대전광역시 중구 대종로 480번길 15',
+    date: '23.09.15',
+    nickname: '도토리45',
+  },
+  {
+    id: 6,
+    imgSrc: 'img/Travelimg1.jpg',
+    title: '디저트 여행3',
+    address: '대전광역시 중구 대종로 480번길 15',
+    date: '23.09.16',
+    nickname: '도토리56',
+  },
+];
 
 export default function TravelCarousel() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
   const settings = {
-    dots: true,
-    infinite: true,
+    dots: false,
+    infinite: false,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
+    nextArrow: (
+      <CustomNextArrow
+        disabled={currentSlide === postdata.length - 3}
+        onClick={() => setCurrentSlide(currentSlide + 1)}
+      />
+    ),
+    prevArrow: <CustomPrevArrow disabled={currentSlide === 0} onClick={() => setCurrentSlide(currentSlide - 1)} />,
+    afterChange: (current: number) => setCurrentSlide(current),
   };
 
   return (
     <CarouselContainer>
       <Slider {...settings}>
-        <ImgContainer>
-          <ImgDiv to="/schedule">
-            <TravelImg src="img/Travelimg1.jpg" alt="여행이미지" />
-          </ImgDiv>
-          <TextDiv>
-            <TitleText>디저트 여행</TitleText>
-            <AddressText>디저트 여행</AddressText>
-            <InfoText>디저트 여행</InfoText>
-            <DataText>디저트 여행</DataText>
-          </TextDiv>
-        </ImgContainer>
-        <ImgContainer>
-          <h3>2</h3>
-        </ImgContainer>
-        <ImgContainer>
-          <h3>3</h3>
-        </ImgContainer>
-        <ImgContainer>
-          <h3>4</h3>
-        </ImgContainer>
-        <ImgContainer>
-          <h3>5</h3>
-        </ImgContainer>
-        <ImgContainer>
-          <h3>6</h3>
-        </ImgContainer>
-        <ImgContainer>
-          <h3>7</h3>
-        </ImgContainer>
-        <ImgContainer>
-          <h3>8</h3>
-        </ImgContainer>
-        <ImgContainer>
-          <h3>9</h3>
-        </ImgContainer>
+        {postdata.map(item => (
+          <ImgContainer key={item.id}>
+            <ImgDiv to="/schedule">
+              <TravelImg src={item.imgSrc} alt="여행이미지" />
+            </ImgDiv>
+            <TextDiv>
+              <TitleText>{item.title}</TitleText>
+              <InfoText>{item.nickname}</InfoText>
+              <AddressText>{item.address}</AddressText>
+              <DataText>{item.date}</DataText>
+            </TextDiv>
+          </ImgContainer>
+        ))}
       </Slider>
     </CarouselContainer>
   );
@@ -59,9 +110,7 @@ const CarouselContainer = styled.div`
   height: 100%;
 `;
 
-const ImgContainer = styled.div`
-  margin: 0 15px;
-`;
+const ImgContainer = styled.div``;
 
 const ImgDiv = styled(Link)``;
 
@@ -74,11 +123,14 @@ const TravelImg = styled.img`
 
 const TextDiv = styled.div`
   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  display: grid;
+  top: 85%;
+  width: 320px;
+  /* left: 50%;
+  transform: translate(-50%, -50%); */
   text-align: center;
-  color: #fff;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
 `;
 
 const TextStyled = css`
@@ -87,13 +139,15 @@ const TextStyled = css`
 
 const TitleText = styled.h2`
   ${TextStyled}
+  font-size: 25px;
+  font-weight: 700;
 `;
 const AddressText = styled.h2`
   ${TextStyled}
 `;
-const InfoText = styled.h2`
+const InfoText = styled.p`
   ${TextStyled}
 `;
-const DataText = styled.h2`
-  ${TextStyled}
+const DataText = styled.p`
+  ${TextStyled} font-weight: 300;
 `;
