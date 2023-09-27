55cccd4ac2c27a4be9104a22ce0bf803ba49ce02
DESKTOP-JVEBI2D\JIN <parkjin0115@naver.com>
2023-09-27 15:26:05 +0900

    # Feat : 지도 스크립트 추가

---
+++ src/component/ScheduleMap.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const KAKAO_API_KEY = '2cc45017695a59169a1f649bdc77f123';
+
+const ScheduleMapLoader = () => {
+  
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_API_KEY}&libraries=services`;
+    script.onload = () => {
+      kakao.maps.load(() => {
+        const container = document.getElementById('map') as HTMLElement; // 맵을 표시할 DOM 엘리먼트
+        const options = {
+          center: new kakao.maps.LatLng(37.5665, 126.9780), // 초기 지도 중심 좌표
+          level: 3, // 지도 확대 레벨
+        };
+        const map = new kakao.maps.Map(container, options);
+      });
+    };
+
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+    }, []);
+
+
+  return(
+    <Con id="map" style={{width : '80%', height: '400px'}}>
+    </Con>
+  )
+}
+
+export default ScheduleMapLoader
+
+const Con = styled.div`
+  margin: 5% auto;
+`;
