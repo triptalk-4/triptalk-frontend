6ccc4d8bf0417b93de63db7d84ca5085baa16edf
Seung Lee <leesh2985@naver.com>
2023-10-02 01:04:32 +0900

    # Feat : 3D캐러셀 사용

--- src/page/TravelPage/CarouselBox/TravelCarousel.tsx
+++ src/page/TravelPage/CarouselBox/TravelCarousel.tsx
@@ -1,161 +1,212 @@
-import { Link } from 'react-router-dom';
-import Slider from 'react-slick';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
 import styled, { css } from 'styled-components';
-import CustomNextArrow from './CustomNextArrow';
-import CustomPrevArrow from './CustomPrevArrow';
-import { useState } from 'react';
+import { MAIN_COLOR } from '../../../color/color';
+import { Link } from 'react-router-dom';
 
-interface PostItem {
-  id: number;
-  imgSrc: string;
-  title: string;
-  address: string;
-  date: string;
-  nickname: string;
-}
-
-const postdata: PostItem[] = [
+const travelData = [
   {
-    id: 1,
-    imgSrc: 'img/Travelimg1.jpg',
-    title: '디저트 여행',
-    address: '대전광역시 중구 대종로 480번길 15',
-    date: '23.09.11',
-    nickname: '도토리',
+    imageUrl: 'img/Travelimg2.jpg',
+    title: '성심당 빵 털기',
+    nickname: '도토리1',
+    address: '대전광역시 중구 대종로480번길 15',
+    date: '23.09.25',
   },
   {
-    id: 2,
-    imgSrc: 'img/Travelimg2.jpg',
-    title: '디저트 여행2',
-    address: '대전광역시 중구 대종로 480번길 15',
-    date: '23.09.12',
-    nickname: '도토리12',
+    imageUrl: 'img/Travelimg1.jpg',
+    title: '서울 궁궐',
+    nickname: '도토리2',
+    address: '대전광역시 중구 대종로480번길 15',
+    date: '23.09.26',
   },
   {
-    id: 3,
-    imgSrc: 'img/Travelimg5.jpg',
-    title: '디저트 여행3',
-    address: '대전광역시 중구 대종로 480번길 15',
-    date: '23.09.13',
-    nickname: '도토리23',
+    imageUrl: 'img/Travelimg5.jpg',
+    title: '뚜벅이',
+    nickname: '도토리3',
+    address: '대전광역시 중구 대종로480번길 15',
+    date: '23.09.27',
   },
   {
-    id: 4,
-    imgSrc: 'img/Travelimg2.jpg',
-    title: '디저트 여행',
-    address: '대전광역시 중구 대종로 480번길 15',
-    date: '23.09.14',
-    nickname: '도토리34',
+    imageUrl: 'img/Travelimg1.jpg',
+    title: '꿈돌이',
+    nickname: '도토리4',
+    address: '대전광역시 중구 대종로480번길 15',
+    date: '23.09.28',
   },
   {
-    id: 5,
-    imgSrc: 'img/Travelimg5.jpg',
-    title: '디저트 여행2',
-    address: '대전광역시 중구 대종로 480번길 15',
-    date: '23.09.15',
-    nickname: '도토리45',
+    imageUrl: 'img/Travelimg2.jpg',
+    title: '서울 궁궐',
+    nickname: '도토리5',
+    address: '대전광역시 중구 대종로480번길 15',
+    date: '23.09.29',
   },
   {
-    id: 6,
-    imgSrc: 'img/Travelimg1.jpg',
-    title: '디저트 여행3',
-    address: '대전광역시 중구 대종로 480번길 15',
-    date: '23.09.16',
-    nickname: '도토리56',
+    imageUrl: 'img/Travelimg5.jpg',
+    title: '힐링',
+    nickname: '도토리6',
+    address: '대전광역시 유성구 봉명동 574',
+    date: '23.09.30',
+  },
+  {
+    imageUrl: 'img/Travelimg1.jpg',
+    title: '비건 맛집',
+    nickname: '도토리7',
+    address: '대전 유성구 농대로 15 신협3층',
+    date: '23.10.01',
+  },
+  {
+    imageUrl: 'img/Travelimg2.jpg',
+    title: '글루텐프리빵집들~',
+    nickname: '도토리8',
+    address: '대전광역시 유성구 지족동 917-5',
+    date: '23.10.02',
+  },
+  {
+    imageUrl: 'img/Travelimg5.jpg',
+    title: '서울 대전 빵털기',
+    nickname: '도토리9',
+    address: '대전 유성구 온천북로33번길 22-3 101호',
+    date: '23.10.03',
   },
 ];
 
 export default function TravelCarousel() {
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  const settings = {
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    nextArrow: (
-      <CustomNextArrow
-        disabled={currentSlide === postdata.length - 3}
-        onClick={() => setCurrentSlide(currentSlide + 1)}
-      />
-    ),
-    prevArrow: <CustomPrevArrow disabled={currentSlide === 0} onClick={() => setCurrentSlide(currentSlide - 1)} />,
-    afterChange: (current: number) => setCurrentSlide(current),
-  };
-
   return (
-    <CarouselContainer>
-      <Slider {...settings}>
-        {postdata.map(item => (
-          <ImgContainer key={item.id}>
-            <ImgDiv>
-              <ImgLink to="/schedule">
-                <TravelImg src={item.imgSrc} alt="여행이미지" />
-              </ImgLink>
-            </ImgDiv>
-            <TextDiv>
-              <TitleText>{item.title}</TitleText>
-              <InfoText>{item.nickname}</InfoText>
-              <AddressText>{item.address}</AddressText>
-              <DataText>{item.date}</DataText>
-            </TextDiv>
-          </ImgContainer>
+    <>
+      <StyledSwiper
+        effect={'coverflow'}
+        grabCursor={false}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: -100,
+          depth: 500,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={false}
+        navigation={true}
+        modules={[EffectCoverflow, Pagination, Navigation]}>
+        {travelData.map((item, index) => (
+          <StyledSwiperSlide key={index}>
+            <Link to="/schedule">
+              <SwiperImage src={item.imageUrl} />
+              {/* 나중에 SwiperImg컨포넌트 추가 예정 */}
+              <SwiperText>
+                <SwiperTopText>
+                  <SwiperTitle>{item.title}</SwiperTitle>
+                  <SwiperNickname>
+                    <MyProfile></MyProfile>
+                    {item.nickname}
+                  </SwiperNickname>
+                </SwiperTopText>
+                <SwiperBottomText>
+                  <SwiperAddress>{item.address}</SwiperAddress>
+                  <SwiperDate>{item.date}</SwiperDate>
+                </SwiperBottomText>
+              </SwiperText>
+            </Link>
+          </StyledSwiperSlide>
         ))}
-      </Slider>
-    </CarouselContainer>
+      </StyledSwiper>
+      <style>
+        {`
+        /* 버튼 스타일 */
+        .swiper-button-prev { 
+          color: ${MAIN_COLOR};         
+        }
+
+        .swiper-button-next {
+          color: ${MAIN_COLOR}; 
+        }
+      `}
+      </style>
+    </>
   );
 }
 
-const CarouselContainer = styled.div`
+const StyledSwiper = styled(Swiper)`
   width: 100%;
-  height: 100%;
+  padding-top: 50px;
+  padding-bottom: 200px;
+`;
+
+const StyledSwiperSlide = styled(SwiperSlide)`
+  background-position: center;
+  background-size: cover;
+  width: 400px;
+  height: 400px;
+`;
+
+const SwiperImage = styled.img`
+  display: block;
+  width: 100%;
+  cursor: pointer;
   position: relative;
 `;
 
-const ImgContainer = styled.div`
-  padding: 30px;
-  position: relative;
-`;
-
-const ImgDiv = styled.div``;
-
-const ImgLink = styled(Link)``;
-
-const TravelImg = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  background: linear-gradient(black, yellow); // 그라데이션 test
-`;
-
-const TextDiv = styled.div`
+const SwiperText = styled.div`
   position: absolute;
-  display: grid;
-  justify-items: stretch;
-  text-align: center;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr 1fr;
-  bottom: 0;
-  width: 320px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  bottom: -200px;
+  width: 100%;
+  height: 100%;
+  padding: 0 15px 10px;
+  background: linear-gradient(to top, rgba(0, 0, 0, 100) 0%, rgba(0, 0, 0, 0) 100%);
 `;
 
-const TextStyled = css`
+const SwiperTopText = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const SwiperBottomText = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const TextColor = css`
   color: #fff;
 `;
 
-const TitleText = styled.h2`
-  ${TextStyled}
+const SwiperTitle = styled.div`
+  ${TextColor}
   font-size: 25px;
-  font-weight: 700;
 `;
-const AddressText = styled.h2`
-  ${TextStyled}
-  text-align: left;
+
+const MyProfile = styled.div`
+  width: 35px;
+  height: 35px;
+  border-radius: 100%;
+  background-color: #fff;
+  margin-right: 10px;
 `;
-const InfoText = styled.p`
-  ${TextStyled}
+
+const SwiperNickname = styled.div`
+  ${TextColor}
+  display: flex;
+  align-items: center;
 `;
-const DataText = styled.p`
-  ${TextStyled} font-weight: 300;
+
+const SwiperAddress = styled.div`
+  ${TextColor}
+  font-weight: 300;
+`;
+
+const SwiperDate = styled.div`
+  ${TextColor}
+  font-size: 15px;
+  font-weight: 300;
 `;
