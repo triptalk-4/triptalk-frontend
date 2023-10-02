8f37aaa424e573da9f7ae53bc47c63a217a4a8b4
Seung Lee <leesh2985@naver.com>
2023-10-02 13:21:07 +0900

    # Design : 글자 찌그러지는거 수정

--- src/component/TopButton/TopButton.tsx
+++ src/component/TopButton/TopButton.tsx
@@ -1,26 +1,31 @@
 import styled from 'styled-components';
 import { MAIN_COLOR } from '../../color/color';
+import { FaArrowUp } from 'react-icons/fa';
 
 function TopButton() {
   const MoveTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
-  return <StyledTopButton onClick={MoveTop}>맨위로</StyledTopButton>;
+  return (
+    <StyledTopButton onClick={MoveTop}>
+      <FaArrowUp />
+    </StyledTopButton>
+  );
 }
 
 export default TopButton;
 
 const StyledTopButton = styled.button`
   position: fixed;
-  font-size: 14px;
+  font-size: 25px;
   bottom: 20px;
   right: 20px;
   background-color: ${MAIN_COLOR};
-  color: white;
+  color: #fff;
   border: none;
   border-radius: 50%;
-  width: 50px;
-  height: 50px;
+  width: 40px;
+  height: 40px;
   cursor: pointer;
   display: flex;
   align-items: center;
