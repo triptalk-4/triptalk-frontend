f9d5255e7017b04fbe4b85d8fecd0cfc837e5ff9
Seung Lee <leesh2985@naver.com>
2023-10-02 13:22:40 +0900

    Revert "# Design : 글자 찌그러지는거 수정"
    This reverts commit 8f37aaa424e573da9f7ae53bc47c63a217a4a8b4.

--- src/component/TopButton/TopButton.tsx
+++ src/component/TopButton/TopButton.tsx
@@ -1,31 +1,26 @@
 import styled from 'styled-components';
 import { MAIN_COLOR } from '../../color/color';
-import { FaArrowUp } from 'react-icons/fa';
 
 function TopButton() {
   const MoveTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
-  return (
-    <StyledTopButton onClick={MoveTop}>
-      <FaArrowUp />
-    </StyledTopButton>
-  );
+  return <StyledTopButton onClick={MoveTop}>맨위로</StyledTopButton>;
 }
 
 export default TopButton;
 
 const StyledTopButton = styled.button`
   position: fixed;
-  font-size: 25px;
+  font-size: 14px;
   bottom: 20px;
   right: 20px;
   background-color: ${MAIN_COLOR};
-  color: #fff;
+  color: white;
   border: none;
   border-radius: 50%;
-  width: 40px;
-  height: 40px;
+  width: 50px;
+  height: 50px;
   cursor: pointer;
   display: flex;
   align-items: center;
