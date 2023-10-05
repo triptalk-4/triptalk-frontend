25efa4a3f44bca1076dbee6cb9c4826debdd4466
Seung Lee <leesh2985@naver.com>
2023-10-05 14:06:37 +0900

    # Docs : 게시물과 분리

--- src/mocks/infoHandlers.ts
+++ src/mocks/infoHandlers.ts
@@ -1,26 +1,17 @@
 import { rest } from 'msw';
 
-const userData = {
-  imageUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+interface userInfoItem {
+  imgUrl: string;
+  nickname: string;
+}
+
+const userInfoData = {
+  imgUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
   nickname: 'User 1',
 };
 
-const postsData = {
-  id: 1,
-  imageSrc: 'img/postimg1.jpg',
-  title: '궁궐 달빛기행',
-  schedule: '23.09.07~23.09.10',
-  date: '23.09.11',
-};
-
 export const infoHandlers = [
-  rest.get('/api/myinfo', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(userData));
-  }),
-];
-
-export const postsHandlers = [
-  rest.get('/api/posts', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(postsData));
+  rest.get<userInfoItem>('/api/myinfo', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(userInfoData));
   }),
 ];
