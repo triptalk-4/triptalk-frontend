d1b81f8cecdb7c5401bfef0bc3e3ca7b397c09b7
Jeong-DongYoung <school2013_@naver.com>
2023-10-05 16:05:15 +0900

    # Docs:  handler 추가

--- src/mocks/setup.ts
+++ src/mocks/setup.ts
@@ -1,8 +1,9 @@
 import { setupWorker } from 'msw';
 import { loginHandlers } from './loginHandlers';
 import { infoHandlers } from './infoHandlers';
+import { scheduleHandler } from './ScheduleHandlers';
 
-export const worker = setupWorker(...loginHandlers, ...infoHandlers);
+export const worker = setupWorker(...loginHandlers, ...infoHandlers, ...scheduleHandler);
 
 if (process.env.NODE_ENV === 'development') {
   worker.start();
