29061e6c1f2b1044bebbec2a203c3a50811d4799
Seung Lee <leesh2985@naver.com>
2023-10-05 22:14:09 +0900

    # Docs : infoEditHandlers 추가

--- src/mocks/setup.ts
+++ src/mocks/setup.ts
@@ -1,10 +1,10 @@
 import { setupWorker } from 'msw';
 import { loginHandlers } from './loginHandlers';
-import { infoHandlers } from './infoHandlers';
 import { scheduleHandler } from './ScheduleHandlers';
 import { postsHandlers } from './postsHandlers';
+import { infoEditHandlers } from './infoEidtHandlers';
 
-export const worker = setupWorker(...loginHandlers, ...infoHandlers, ...scheduleHandler, ...postsHandlers);
+export const worker = setupWorker(...loginHandlers, ...scheduleHandler, ...postsHandlers, ...infoEditHandlers);
 
 if (process.env.NODE_ENV === 'development') {
   worker.start();
