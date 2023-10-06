95ca5b902e3315dc053716e824c3bbbbc4794b7a
DESKTOP-JVEBI2D\JIN <parkjin0115@naver.com>
2023-10-06 18:33:01 +0900

    #Docs : 로그인 성공시 userInfo를 로컬스토어에 저장되게함

--- src/mocks/loginHandlers.ts
+++ src/mocks/loginHandlers.ts
@@ -7,17 +7,20 @@ interface LoginRequestBody {
 
 const userData = [
   {
-    id: 1,
+    imgUrl: '',
+    nickname : '제로',
     email: 'user1@naver.com',
     password: 'Password123'
   },
   {
-    id: 2,
+    imgUrl: '',
+    nickname : '콜라',
     email: 'user2@naver.com',
     password: 'Password456'
   },
   {
-    id: 3,
+    imgUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+    nickname : '제로사이다',
     email: 'user3@naver.com',
     password: 'Password789'
   },
@@ -30,7 +33,16 @@ export const loginHandlers = [
     const user = userData.find((u) => u.email === email && u.password === password)
 
     if (user) {
-      return res(ctx.status(200), ctx.json({ userId: user.id }));
+      const userInfo = {
+        imageUrl : user.imgUrl,
+        email: user.email,
+        nickName: user.nickname,
+        password: user.password,
+      }
+
+      localStorage.setItem('userInfo', JSON.stringify(userInfo))
+
+      return res(ctx.status(200), ctx.json({ userId: user.email }));
     } else {
       return res(ctx.status(401), ctx.json({ message: '로그인 실패' }))
     }
