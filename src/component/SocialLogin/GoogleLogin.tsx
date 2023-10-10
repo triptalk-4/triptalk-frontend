656b148238370d42568198d74514b714d9edbefd
DESKTOP-JVEBI2D\JIN <parkjin0115@naver.com>
2023-10-10 17:27:46 +0900

    #Test  : 구글로그인 토큰 받아오기 계속 테스트중

--- src/component/SocialLogin/GoogleLogin.tsx
+++ src/component/SocialLogin/GoogleLogin.tsx
@@ -4,23 +4,52 @@ import axios from 'axios';
 import { useGoogleLogin } from '@react-oauth/google';
 import { useNavigate } from 'react-router';
 
+interface GoogleLoginResponse {
+  code: string;
+}
+
 const GoogleLogin = () => {
   const navigate = useNavigate();
 
+  const onSuccess = async ({ code }: GoogleLoginResponse) => {
+    try {
+      const { accessToken } = await exchangeCodeForAccessToken(code);
+      console.log(accessToken);
+      navigate('/main');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const googleSocialLogin = useGoogleLogin({
     scope: 'email profile',
-    onSuccess: async ({ code }: any) => {
-      axios.post('http://52.79.200.55:8080/api/auth/google', { code }).then(({ data }) => {
-        console.log(data);
-
-        navigate('/main');
-      });
-    },
+    onSuccess,
     onError: error => {
-      console.error(error);
+      console.error('error다', error);
     },
     flow: 'auth-code',
   });
+
+  const exchangeCodeForAccessToken = async (code: any) => {
+    try {
+      const clientID = '57409677042-8qrpnbbfcq8d6jeq9kmvh8357vi4p4up.apps.googleusercontent.com';
+      const clientSecret = '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';
+
+      const response = await axios.post('http://52.79.200.55:8080/api/auth/google', {
+        code,
+        client_id: clientID,
+        client_secret: clientSecret,
+        redirect_uri: 'http://ec2-52-79-200-55.ap-northeast-2.compute.amazonaws.com:8080/api/auth/google',
+        grant_ype: 'authorization_code',
+      });
+
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw new Error('실패');
+    }
+  };
 
   return (
     <Google onClick={googleSocialLogin}>
