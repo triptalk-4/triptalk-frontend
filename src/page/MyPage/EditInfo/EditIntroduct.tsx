122114b38d15ebbb4520d6e7ec1560e912e5d192
Seung Lee <leesh2985@naver.com>
2023-10-10 21:31:46 +0900

    # Refactor : 백앤드 연동

--- src/page/MyPage/EditInfo/EditIntroduct.tsx
+++ src/page/MyPage/EditInfo/EditIntroduct.tsx
@@ -1,13 +1,45 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { SUPER_LIGHT_ORANGE_COLOR } from '../../../color/color';
+import axios from 'axios';
+import { RootState } from '../../../store/store';
+import { useSelector } from 'react-redux';
 
 interface EditIntroProps {
   onUpdateIntro: (text: string) => void;
 }
 
 export default function EditIntroduct({ onUpdateIntro }: EditIntroProps) {
-  const [userIntro, setUserIntro] = useState('사용자의 소개 내용을 입력하세요');
+  // const [userIntro, setUserIntro] = useState('사용자의 소개 내용을 입력하세요');
+  const [userIntro, setUserIntro] = useState('');
+
+  const token = useSelector((state: RootState) => state.token.token); // Redux에서 토큰 가져오기
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const fetchUserInfo = async () => {
+      try {
+        const response = await axios.get('http://52.79.200.55:8080/api/users/profile', {
+          headers: {
+            Authorization: `Bearer ${token}`, //필수
+          },
+        });
+
+        if (response.data) {
+          const { aboutMe } = response.data;
+
+          setUserIntro(aboutMe);
+        } else {
+          console.log(response);
+          alert('사용자 정보가 없습니다 로그인확인해주세요');
+        }
+      } catch (error) {
+        console.error('사용자 정보 가져오기 오류 확인바람:', error);
+      }
+    };
+
+    fetchUserInfo(); // 비동기 함수 호출
+  }, [token, userIntro]);
 
   const handleIntroChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     const newText = e.target.value;
