2dd2ef9d442d4923df146368ef23979e1ca766d6
Seung Lee <leesh2985@naver.com>
2023-10-23 23:17:34 +0900

    # Docs : 스크롤 해제

--- src/page/MyPage/MyInfoSaved.tsx
+++ src/page/MyPage/MyInfoSaved.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 import MySaved from './MySaved';
@@ -13,53 +13,12 @@ interface Save {
   plannerId: number;
 }
 
-const PAGE_SIZE = 6;
-
 export default function MyInfoSaved() {
   const [savedData, setSavedData] = useState<Save[]>([]); // msw
   const [containerClassName, setContainerClassName] = useState('flex-start');
   const [isLoading, setIsLoading] = useState(false); // 로딩 상태
-  const [isEndPage, setIsEndPage] = useState(false); // 페이지 끝 상태
-  const targetRef = useRef<HTMLDivElement | null>(null);
-  const observerRef = useRef<IntersectionObserver | null>(null);
-  const [page, setPage] = useState(0);
-
-  // useEffect(() => {
-  //   fetch('/address/api/saved')
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       setSavedData(data.slice(0, 6));
-  //     })
-  //     .catch(error => console.error('가짜 API 요청 실패:', error));
-  // }, []);
-
-  useEffect(() => {
-    const fetchUserSaved = async () => {
-      if (isEndPage || 0 === page) return;
-      setIsLoading(true);
-      const Access_token = localStorage.getItem('token');
-      try {
-        const response = await axios.get(`/address/api/users/planners/userSave?page=${page}&pageSize=1`, {
-          headers: {
-            Authorization: `Bearer ${Access_token}`,
-          },
-        });
-        setIsLoading(false);
-        const newData = response.data.content;
-        if (newData.length === 0) {
-          setIsEndPage(true);
-        } else {
-          const threeItems = newData.slice(0, PAGE_SIZE);
-          setSavedData(prevData => [...prevData, ...threeItems]);
-        }
-      } catch (error) {
-        setIsLoading(false);
-        console.error('데이터 요청 실패:', error);
-      }
-    };
-    console.log('savedData', savedData);
-    fetchUserSaved();
-  }, [page, isEndPage]);
+  // const [isEndPage, setIsEndPage] = useState(false);
+  // const [page, setPage] = useState(0);
 
   useEffect(() => {
     if (savedData.length >= 2) {
@@ -70,45 +29,41 @@ export default function MyInfoSaved() {
   }, [savedData]);
 
   useEffect(() => {
-    const observer = new IntersectionObserver(handleIntersection, {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.5,
-    });
-    observerRef.current = observer; // useRef로 observer 저장
-    if (targetRef.current) {
-      observer.observe(targetRef.current);
-    }
-    return () => {
-      if (targetRef.current) {
-        observer.unobserve(targetRef.current);
+    const fetchData = async () => {
+      if (isLoading) return; // 이미 로딩 중이라면 중복 요청 방지
+
+      setIsLoading(true);
+
+      try {
+        const Access_token = localStorage.getItem('token');
+        const response = await axios.get(`/address/api/users/planners/userSave?page=0&pageSize=100`, {
+          headers: {
+            Authorization: `Bearer ${Access_token}`,
+          },
+        });
+
+        const newData = response.data.content;
+
+        if (newData.length === 0) {
+          // setIsEndPage(true);
+        } else {
+          setSavedData(newData);
+        }
+      } catch (error) {
+        console.error('데이터 요청 실패:', error);
+      } finally {
+        setIsLoading(false);
       }
     };
-  }, [page, isLoading]);
 
-  function handleIntersection(entries: IntersectionObserverEntry[]) {
-    entries.forEach(entry => {
-      if (entry.isIntersecting && !isLoading) {
-        setPage(prevPage => {
-          if (!isLoading) {
-            return prevPage + 1;
-          }
-          return prevPage;
-        });
-        if (targetRef.current && observerRef.current) {
-          observerRef.current.unobserve(targetRef.current);
-        }
-        console.log(page);
-      }
-    });
-  }
+    fetchData();
+  }, []);
 
   return (
     <SavedContainer className={containerClassName}>
       {savedData.map(item => (
         <MySaved key={item.plannerId} savedData={item} />
       ))}
-      {!isEndPage && <ObserverTarget ref={targetRef} />}
       {isLoading && <LoadingMessage>로딩 중...</LoadingMessage>}
     </SavedContainer>
   );
@@ -128,11 +83,6 @@ const SavedContainer = styled.div`
   &.space-between {
     justify-content: space-between;
   }
-`;
-
-const ObserverTarget = styled.div`
-  width: 100%;
-  height: 1px; /* 교차 영역을 감지할 빈 요소 */
 `;
 
 const LoadingMessage = styled.p`
