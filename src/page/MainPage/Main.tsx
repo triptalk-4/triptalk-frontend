ca29d6145dbb4d7e2a52f688eeb66b1fb3dfec65
Seung Lee <leesh2985@naver.com>
2023-10-24 18:18:35 +0900

    # Docs : 주석 및 사용안한는 css 삭제

--- src/page/MainPage/Main.tsx
+++ src/page/MainPage/Main.tsx
@@ -29,17 +29,6 @@ const Main = () => {
         </TopTitle>
         <LikeCarousel />
       </TopPosts>
-      {/* <AddPost>
-        <AddImg src="img/addimg.jpg" alt="관광지 이미지" />
-      </AddPost> */}
-      {/* <MainImgContainer onClick={handleMainImgClick}>
-        <MainImageWrapper>
-          <MainImage src="img/Boast.jpg" alt="여행가는 느낌의 이미지" />
-          <MainImageCaption>
-            여러분의 여행지를 <br /> 'Trip Talk'에 보여주세요
-          </MainImageCaption>
-        </MainImageWrapper>
-      </MainImgContainer> */}
     </MainContainer>
   );
 };
@@ -70,45 +59,6 @@ const TileSpan = styled.span`
   font-size: 15px;
   font-weight: 300;
   color: ${GRAY_COLOR};
-`;
-
-// const AddPost = styled.div`
-//   margin: 80px auto;
-// `;
-//
-// const AddImg = styled.img`
-//   width: 100%;
-//   height: auto;
-// `;
-
-const MainImgContainer = styled.div`
-  width: 100%;
-  margin-top: 10%;
-  cursor: pointer;
-  height: 445px;
-`;
-
-const MainImageWrapper = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100%;
-`;
-
-const MainImage = styled.img`
-  width: 100%;
-  object-fit: contain;
-`;
-
-const MainImageCaption = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  background-color: rgba(0, 0, 0, 0.25);
-  color: white;
-  padding: 25px;
-  font-size: 50px;
-  text-align: center;
-  transform: translate(-50%, -50%);
 `;
 
 export default Main;
