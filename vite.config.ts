413a90c210292d0f2ce1562b7deaf16ab5622be8
DESKTOP-JVEBI2D\JIN <parkjin0115@naver.com>
2023-10-24 13:51:27 +0900

    #Test : production 상태일때 코드로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,28 @@ import react from '@vitejs/plugin-react';
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 // https://vitejs.dev/config/
+const developmentProxy = {
+  '/address': {
+    target: 'https://triptalk.xyz',
+    changeOrigin: true,
+    rewrite: path => path.replace(/^\/address/, ''),
+    secure: false,
+    ws: true,
+  }
+}
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: isDevelopment ? {
-      // '/api': 'http://52.79.200.55:8080',
-      '/address': {
-        target: 'https://triptalk.xyz',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/address/, ''),
-        secure: false,
-        ws: true,
-      },
-    } : {},
+    proxy: isDevelopment ? developmentProxy : {},
     port: 3000,
   },
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  }
 });
