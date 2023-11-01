468a657a1c565b7fdfc1f1bca5c9d9a512c2f2e7
Seung Lee <leesh2985@naver.com>
2023-11-01 13:31:55 +0900

    # Docs: 하위 컴포넌트에 정보 주기

--- src/page/TravelPage/TravelMap.tsx
+++ src/page/TravelPage/TravelMap.tsx
@@ -3,6 +3,55 @@ import { GRAY_COLOR } from '../../color/color';
 import ReviewMap from '../../component/ReviewMap';
 import TravelPosts from './TravelPosts';
 
+const defaultTravelsData = [
+  // 서울
+  {
+    imgUrl: 'img/Travelimg1.jpg',
+    title: '서울 여행 1',
+    nickname: '모험가',
+    address: '서울시 강남구 강남대로 123번길 45',
+    date: '10/01/2023',
+    heartCount: 5,
+    lookUpCount: 5,
+  },
+  {
+    imgUrl: 'img/Travelimg2.jpg',
+    title: '서울 여행 2',
+    nickname: '여행가2',
+    address: '서울시 강북구 강북로 789번길 10',
+    date: '10/05/2023',
+    heartCount: 10,
+    lookUpCount: 7,
+  },
+  {
+    imgUrl: 'img/Travelimg3.jpg',
+    title: '서울 힐링 여행',
+    nickname: '힐링객',
+    address: '서울시 마포구 마포로 56',
+    date: '10/10/2023',
+    heartCount: 8,
+    lookUpCount: 12,
+  },
+  {
+    imgUrl: 'img/Travelimg4.jpg',
+    title: '서울 문화 탐방',
+    nickname: '문화인',
+    address: '서울시 종로구 종로 1010',
+    date: '11/05/2023',
+    heartCount: 9,
+    lookUpCount: 15,
+  },
+  {
+    imgUrl: 'img/Travelimg5.jpg',
+    title: '서울 도심 힐링',
+    nickname: '도시탐험',
+    address: '서울시 중구 명동길 567',
+    date: '11/15/2023',
+    heartCount: 12,
+    lookUpCount: 9,
+  },
+];
+
 export default function TravelMap() {
   return (
     <TravelContainer>
@@ -13,7 +62,7 @@ export default function TravelMap() {
         <ReviewMap onPlacesSelected={() => {}} />
       </Map>
       <PostBorder></PostBorder>
-      <TravelPosts />
+      <TravelPosts travelDatas={defaultTravelsData} />
     </TravelContainer>
   );
 }
