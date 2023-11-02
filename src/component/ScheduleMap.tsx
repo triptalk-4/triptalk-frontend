28ef3b02df266831be110af5a0f56a982bc68dd7
DESKTOP-JVEBI2D\JIN <parkjin0115@naver.com>
2023-11-02 21:17:04 +0900

    #Refactor : 사용자가 입력한 장소가 coreContainer에 보이게 코드 수정

--- src/component/ScheduleMap.tsx
+++ src/component/ScheduleMap.tsx
@@ -43,7 +43,7 @@ const ScheduleMapLoader: React.FC<SchduleMapLoaderProps> = ({ onPlacesSelected, 
         const container = document.getElementById('map') as HTMLElement; // 맵을 표시할 DOM 엘리먼트
         const options = {
           center: new kakao.maps.LatLng(37.5665, 126.978), // 초기 지도 중심 좌표
-          level: 6, // 지도 확대 레벨
+          level: 5, // 지도 확대 레벨
         };
         const newMap = new kakao.maps.Map(container, options);
         setMap(newMap);
@@ -94,93 +94,90 @@ const ScheduleMapLoader: React.FC<SchduleMapLoaderProps> = ({ onPlacesSelected, 
     }
   }, [onPlace, mapPings]);
 
-  // const handleSearch = async () => {
-  //   if (map) {
-  //     const ps = new kakao.maps.services.Places();
+  const handleSearch = async () => {
+    if (map) {
+      const ps = new kakao.maps.services.Places();
 
-  //     try {
-  //       // 입력한 키워드로 장소를 검색
-  //       const data = await new Promise<PlaceSearchResult[]>((resolve, reject) => {
-  //         ps.keywordSearch(searchPlace, (data, status) => {
-  //           if (status === kakao.maps.services.Status.OK) {
-  //             resolve(data);
-  //           } else {
-  //             reject(status);
-  //           }
-  //         });
-  //       });
+      try {
+        // 입력한 키워드로 장소를 검색
+        const data = await new Promise<PlaceSearchResult[]>((resolve, reject) => {
+          ps.keywordSearch(searchPlace, (data, status) => {
+            if (status === kakao.maps.services.Status.OK) {
+              resolve(data);
+            } else {
+              reject(status);
+            }
+          });
+        });
 
-  //       const bounds = new kakao.maps.LatLngBounds();
-  //       const newPlaceInfos: PlaceInfo[] = [];
+        const bounds = new kakao.maps.LatLngBounds();
+        const newPlaceInfos: PlaceInfo[] = [];
 
-  //       for (let i = 0; i < 1; i++) {
-  //         const place = data[0];
+        for (let i = 0; i < 1; i++) {
+          const place = data[0];
 
-  //         if (place) {
-  //           const selectedPlaceInfo: PlaceInfo = {
-  //             position: {
-  //               lat: Number(place.y),
-  //               lng: Number(place.x),
-  //             },
-  //             addressName: place.address_name,
-  //             placeName: place.place_name,
-  //             roadAddressName: place.road_address_name,
-  //           };
+          if (place) {
+            const selectedPlaceInfo: PlaceInfo = {
+              position: {
+                lat: Number(place.y),
+                lng: Number(place.x),
+              },
+              addressName: place.address_name,
+              placeName: place.place_name,
+              roadAddressName: place.road_address_name,
+            };
 
-  //           newPlaceInfos.push(selectedPlaceInfo);
-  //         }
+            newPlaceInfos.push(selectedPlaceInfo);
+          }
 
-  //         // 마커를 생성하고 지도에 표시
-  //         const marker = new kakao.maps.Marker({
-  //           map,
-  //           position: new kakao.maps.LatLng(Number(place.y), Number(place.x)),
-  //         });
-  //         kakao.maps.event.addListener(marker, 'click', () => {
-  //           const infowindow = new kakao.maps.InfoWindow({
-  //             content: `<div style="padding:5px; font-size:12px">${place.place_name}</div>`,
-  //           });
-  //           infowindow.open(map, marker);
-  //         });
+          // 마커를 생성하고 지도에 표시
+          const marker = new kakao.maps.Marker({
+            map,
+            position: new kakao.maps.LatLng(Number(place.y), Number(place.x)),
+          });
+          kakao.maps.event.addListener(marker, 'click', () => {
+            const infowindow = new kakao.maps.InfoWindow({
+              content: `<div style="padding:5px; font-size:12px">${place.place_name}</div>`,
+            });
+            infowindow.open(map, marker);
+          });
 
-  //         bounds.extend(new kakao.maps.LatLng(Number(place.y), Number(place.x)));
-  //       }
+          bounds.extend(new kakao.maps.LatLng(Number(place.y), Number(place.x)));
+        }
 
-  //       // 이전 상태와 새로운 장소 정보를 합치고 상태를 업데이트
-  //       setSelectedPlaceInfos(prevPlaceInfos => {
-  //         const updatedPlaceInfos = [...prevPlaceInfos, ...newPlaceInfos];
-  //         onPlacesSelected(updatedPlaceInfos);
-  //         return updatedPlaceInfos;
-  //       });
+        // 이전 상태와 새로운 장소 정보를 합치고 상태를 업데이트
+        setSelectedPlaceInfos(prevPlaceInfos => {
+          const updatedPlaceInfos = [...prevPlaceInfos, ...newPlaceInfos];
+          onPlacesSelected(updatedPlaceInfos);
+          return updatedPlaceInfos;
+        });
 
-  //       // 검색된 장소 위치를 기준으로 지도 범위를 재설정
-  //       map.setBounds(bounds);
-  //     } catch (error) {
-  //       console.error('검색 오류:', error);
-  //     }
-  //   }
-  // };
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정
+        map.setBounds(bounds);
+      } catch (error) {
+        console.error('검색 오류:', error);
+      }
+    }
+  };
 
   return (
     <>
-      <S_Title>일정</S_Title>
       <Con
         id="map"
         style={{
           width: '100%',
-          height: '450px',
+          height: '400px',
           border: `1px solid ${YELLOW_COLOR}`,
           borderRadius: '4px',
           margin: '0px',
         }}></Con>
-      {/* {!onPlace ? <Input type="text" placeholder="장소 검색" onChange={e => setSearchPlace(e.target.value)} /> : null}
-      {!onPlace ? <Button onClick={handleSearch}>검색</Button> : null} */}
+      {!onPlace ? <Input type="text" placeholder="장소 검색" onChange={e => setSearchPlace(e.target.value)} /> : null}
+      {!onPlace ? <Button onClick={handleSearch}>검색</Button> : null}
     </>
   );
 };
 
 export default ScheduleMapLoader;
-
-const S_Title = styled.h1``;
 
 const Con = styled.div`
   margin: 5% auto;
@@ -209,7 +206,6 @@ const Button = styled.button`
   border-radius: 4px;
   transition: border-color 0.3s;
   outline: none;
-  cursor: pointer;
   &:focus {
     border-color: ${MAIN_COLOR};
   }
