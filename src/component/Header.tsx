07b968ccd374b01060c597df11e03c619768ff2e
Jeong-DongYoung <school2013_@naver.com>
2023-11-06 15:58:55 +0900

    # Design :  모달창 삭제버튼추가

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -24,9 +24,6 @@ interface userInfoDate {
   username: string;
 }
 
-interface ModalProps {
-  onClose: () => void;
-}
 export default function Header() {
   //  const [userImg, setUserImg] = useState(''); // msw
   const token = useSelector((state: RootState) => state.token.token); // Redux에서 토큰 가져오기
@@ -211,26 +208,4 @@ const Notice = styled.div`
 `;
 const Bell = styled(BiBell)`
   font-size: 23px;
-`;
-
-const ModalBackdrop = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1000;
-`;
-
-const ModalContent = styled.div`
-  width: 700px;
-  height: 600px;
-  background-color: #ffffff;
-  border-radius: 8px;
-  padding: 20px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
 `;
