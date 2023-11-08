97181c3fe6dd9c26dfe4f2dd3924d8556e16417b
Seung Lee <leesh2985@naver.com>
2023-11-08 16:57:24 +0900

    # Design : 반응형 css

--- src/page/MyPage/MyInfo.tsx
+++ src/page/MyPage/MyInfo.tsx
@@ -354,13 +354,8 @@ const UserLogoutBtn = styled.button`
 
 const ContentContainer = styled.div`
   border-top: 1px solid #000;
-  @media (max-width: 1400px) {
-    width: 90%;
-    margin: 0 auto;
-  }
-  @media (max-width: 1250px) {
-    width: 80%;
-  }
+  margin: 0 auto;
+  width: 80%;
 `;
 
 const ContentUl = styled.ul`
